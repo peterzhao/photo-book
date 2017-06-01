4e9cce608945aa7839762af2b04275e6892e2516
peterzhao <peter.qs.zhao@gmail.com>
2017-06-01 09:12:59 -0400

    works basiclly now

--- src/photo-service/index.js
+++ src/photo-service/index.js
@@ -8,25 +8,23 @@ module.exports = (bucket, path, s3 = s3Default) => {
   };
 
   const transform = (photos) => {
-    console.log(JSON.stringify(photos));
     const result = {};
     photos.Contents.map((e) => { return e.Key; })
     .forEach((content) => {
       const indexOfSize = content.indexOf('%2B');
       const name = content.substring(0, indexOfSize);
       const decoration = content.substring(indexOfSize);
-      const key = `https://${s3.endpoint}/${bucket}/${name}`;
       const sizeName = decoration.split('%2B')[1];
       const sizeExtension = decoration.split('%2B')[2];
       const size = sizeExtension.substring(0, sizeExtension.length - 4);
       const width = size.split('x')[0];
       const height = size.split('x')[1];
       const category = content.split('/')[2];
-      if (!result[key]) result[key] = { category };
-      result[key][sizeName] = { path: decoration, w: width, h: height };
+      const url = `https://${s3.endpoint}/${bucket}/${content}`;
+      if (!result[name]) result[name] = { category };
+      result[name][sizeName] = { url, w: width, h: height };
     });
-    console.log(JSON.stringify(result));
-    return result;
+    return Object.keys(result).map((k) => { return result[k]; });
   };
 
   const photoListPromise = new Promise((resolve, reject) => {
--- src/photo-service/test.js
+++ src/photo-service/test.js
@@ -26,20 +26,20 @@ describe('photoService', () => {
         { Key: 'photos/public/Travel/2017/IMG_2%2Blarge%2B1600x1200.jpg' }
       ]
     };
-    const photoList = {
-      'https://s3-ca-central-1/myBucket/photos/public/Portrait/2017/IMG_1': {
-        small: { path: '%2Bsmall%2B180x240.jpg', w: '180', h: '240' },
-        med: { path: '%2Bmed%2B1047x768.jpg', w: '1047', h: '768' },
-        large: { path: '%2Blarge%2B1600x1200.jpg', w: '1600', h: '1200' },
+    const photoList = [
+      {
+        small: { url: 'https://s3-ca-central-1/myBucket/photos/public/Portrait/2017/IMG_1%2Bsmall%2B180x240.jpg', w: '180', h: '240' },
+        med: { url: 'https://s3-ca-central-1/myBucket/photos/public/Portrait/2017/IMG_1%2Bmed%2B1047x768.jpg', w: '1047', h: '768' },
+        large: { url: 'https://s3-ca-central-1/myBucket/photos/public/Portrait/2017/IMG_1%2Blarge%2B1600x1200.jpg', w: '1600', h: '1200' },
         category: 'Portrait'
       },
-      'https://s3-ca-central-1/myBucket/photos/public/Travel/2017/IMG_2': {
-        small: { path: '%2Bsmall%2B180x240.jpg', w: '180', h: '240' },
-        med: { path: '%2Bmed%2B1047x768.jpg', w: '1047', h: '768' },
-        large: { path: '%2Blarge%2B1600x1200.jpg', w: '1600', h: '1200' },
+      {
+        small: { url: 'https://s3-ca-central-1/myBucket/photos/public/Travel/2017/IMG_2%2Bsmall%2B180x240.jpg', w: '180', h: '240' },
+        med: { url: 'https://s3-ca-central-1/myBucket/photos/public/Travel/2017/IMG_2%2Bmed%2B1047x768.jpg', w: '1047', h: '768' },
+        large: { url: 'https://s3-ca-central-1/myBucket/photos/public/Travel/2017/IMG_2%2Blarge%2B1600x1200.jpg', w: '1600', h: '1200' },
         category: 'Travel'
       }
-    };
+    ];
     s3Stub.api.listObjects.callsArgWith(1, undefined, photoListFromS3);
     return assert.eventually.deepEqual(
       photoService('myBucket', 'photos/public', s3Stub).getPhotoList(),
